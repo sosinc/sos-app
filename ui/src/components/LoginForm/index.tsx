e8d73a37b30683615135ab3f311cfa72982761b9
Jatinder <jkhabra09@gmail.com>
2020-05-20 16:38:31 +0530

    feat: add back arrow icon (login form)

--- ui/src/components/LoginForm/index.tsx
+++ ui/src/components/LoginForm/index.tsx
@@ -1,26 +1,42 @@
-import { FormikProps, withFormik } from 'formik';
+import { useFormik } from 'formik';
 import { useState } from 'react';
 import * as Yup from 'yup';
 
 import c from './style.module.scss';
 
-interface FormValues {
-  email: string;
-  password: string;
-}
-
-const Login: React.FC<FormikProps<FormValues>> = (p) => {
+const Login: React.FC = () => {
   const [doEmailExist, setEmail] = useState(false);
   const [emailInputClass, setEmailInputClass] = useState<string>('');
   const [passwordInputClass, setPasswordInputClass] = useState<string>(c.hiddenFieldAtRight);
 
+  const form = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+    },
+
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email('Invalid email address')
+        .required('Required'),
+      password: Yup.string()
+        .max(15, 'Must be 15 characters or less')
+        .required('Required'),
+    }),
+
+    onSubmit: () => {
+      setEmail(true);
+    },
+  });
+
   const changeInputField = () => {
-    if (p.values.email.length) {
+    if (form.values.email.length) {
       setEmailInputClass(c.swipeLeft);
       setPasswordInputClass('');
-      setTimeout(() => {
-        setEmail(true);
-      }, 1000);
+      setEmail(true);
+      // setTimeout(() => {
+      // setEmail(true);
+      // }, 3000);
     }
   };
 
@@ -31,17 +47,17 @@ const Login: React.FC<FormikProps<FormValues>> = (p) => {
   };
 
   const inputClass = (type: 'email' | 'password') => {
-    return p.errors[type] && p.touched[type] ? `${c.formError} ${c.input}` : c.input;
+    return form.errors[type] && form.touched[type] ? `${c.formError} ${c.input}` : c.input;
   };
 
   const errorMessage =
-    p.touched.email && p.errors.email ? (
-      <span className={c.validationMessage}>{p.errors.email}</span>
+    form.touched.email && form.errors.email ? (
+      <span className={c.validationMessage}>{form.errors.email}</span>
     ) : null;
 
   const errorMessagePassword =
-    p.touched.password && p.errors.password ? (
-      <span className={c.validationMessage}>{p.errors.password}</span>
+    form.touched.password && form.errors.password ? (
+      <span className={c.validationMessage}>{form.errors.password}</span>
     ) : null;
 
   const emailField = (
@@ -49,7 +65,7 @@ const Login: React.FC<FormikProps<FormValues>> = (p) => {
       <input
         id="email"
         placeholder="enter your work email"
-        {...p.getFieldProps('email')}
+        {...form.getFieldProps('email')}
         className={`${inputClass('email')}`}
       />
       {errorMessage}
@@ -57,15 +73,21 @@ const Login: React.FC<FormikProps<FormValues>> = (p) => {
   );
 
   const passwordField = (
-    <div className={`${c.inputFieldContainer} ${passwordInputClass}`}>
-      <input
-        id="password"
-        placeholder="enter your password"
-        {...p.getFieldProps('password')}
-        className={`${inputClass('password')}`}
-      />
-      {errorMessagePassword}
-    </div>
+    <span className={`${c.backToEmailField} ${passwordInputClass}`}>
+      <span className={c.arrowIconContainer} onClick={resetInputFields}>
+        <img className={c.arrowIcon} src="/assets/images/leftArrow.svg" alt="back" />
+      </span>
+
+      <div className={`${c.inputFieldContainer}`}>
+        <input
+          id="password"
+          placeholder="enter your password"
+          {...form.getFieldProps('password')}
+          className={`${inputClass('password')}`}
+        />
+        {errorMessagePassword}
+      </div>
+    </span>
   );
 
   const loginButton = doEmailExist ? (
@@ -79,32 +101,14 @@ const Login: React.FC<FormikProps<FormValues>> = (p) => {
   );
 
   return (
-    <form className={c.loginForm} onSubmit={p.handleSubmit}>
+    <form className={c.loginForm} onSubmit={form.handleSubmit}>
       <div className={c.fieldsContainer}>
-        <TextField type="email" name="email" placeholder="" />
-
-        <span className={`${c.backToEmailField} ${passwordInputClass}`}>
-          <button type="button" onClick={resetInputFields}>
-            B
-          </button>
-          <TextField type="password" name="password" placeholder="" />
-        </span>
+        {emailField}
+        {passwordField}
       </div>
       {loginButton}
     </form>
   );
 };
 
-export default withFormik<{}, FormValues>({
-  handleSubmit: (v, b) => {
-    console.warn('SUBMITTING', v, b);
-  },
-  validationSchema: Yup.object({
-    email: Yup.string()
-      .email('Invalid email address')
-      .required('Required'),
-    password: Yup.string()
-      .max(15, 'Must be 15 characters or less')
-      .required('Required'),
-  }),
-})(Login);
+export default Login;
