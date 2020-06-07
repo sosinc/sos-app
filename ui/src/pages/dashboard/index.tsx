05d5b5ede75102b06e3d066adf5ed65ae054fe9a
Kajal Negi <kl.negi03@gmail.com>
2020-06-07 11:46:55 +0530

    feat: add header to sidebar

---
+++ ui/src/pages/dashboard/index.tsx
@@ -0,0 +1,32 @@
+import HEAD from 'next/head';
+import Link from 'next/link';
+import c from './index.module.scss';
+
+const Dashboard = () => {
+  return (
+    <>
+      <HEAD>
+        <title>SOS - Dashboard</title>
+      </HEAD>
+      <div className={c.container}>
+        <div className={c.sidebar}>
+          <div className={c.header}>
+            <Link href="/">
+              <img className={c.logoImage} src="/assets/images/sos-logo.svg" alt="o" />
+            </Link>
+            <img className={c.pic} src="/assets/images/avatar.svg" alt="pic" />
+            <span className={c.contextMenu} />
+          </div>
+          <div className={c.detail}>Top Bar</div>
+          <div className={c.footer}>
+            <span className={c.feedbackIcon} />
+            <a className={c.feedbackText}>Send Feedback</a>
+          </div>
+        </div>
+        <div>Second Half</div>
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
