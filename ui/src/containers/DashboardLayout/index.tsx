b327ffa87fd5ed6aa9bbe6a8c01789f9694086ec
Jatinder <jkhabra09@gmail.com>
2020-06-12 14:39:17 +0530

    fix: sidebar-layout styles

--- ui/src/containers/DashboardLayout/index.tsx
+++ ui/src/containers/DashboardLayout/index.tsx
@@ -1,9 +1,8 @@
 import classNames from 'classnames/bind';
 import Head from 'next/head';
 import Link from 'next/link';
-import { AiOutlineEllipsis } from 'react-icons/ai';
 import { FaUsers } from 'react-icons/fa';
-import { MdBusiness } from 'react-icons/md';
+import { MdBusiness, MdMoreHoriz } from 'react-icons/md';
 
 import WithUser from 'src/containers/WithUser';
 
@@ -28,27 +27,32 @@ const Index: React.FC<LayoutProps> = (p) => {
             <Link href="/">
               <img className={c('logo-image')} src="/assets/images/sos-logo.svg" alt="o" />
             </Link>
-            <div className={c('pic-container')}>
+            <div className={c('avatar-container')}>
               <img className={c('pic')} src="/assets/images/avatar.svg" alt="pic" />
               <span className={c('online-status')} />
             </div>
-            <AiOutlineEllipsis title="more" className={c('dot-menu-icon')} />
+
+            <MdMoreHoriz title="more" className={c('dot-menu-icon')} />
           </div>
-          <div className={c('admin-section')}>
-            <span>Admin</span>
-            <div className={c('organization-container')}>
-              <MdBusiness />
-              <Link href="/organizations">
-                <span className={c('admin-section-title')}>Organizations</span>
-              </Link>
-            </div>
-            <div className={c('employee-container')}>
-              <FaUsers />
-              <Link href="/employees">
-                <span className={c('admin-section-title')}>Employees</span>
-              </Link>
-            </div>
+
+          <div className={c('section')}>
+            <div className={c('header-row')}>Admin</div>
+
+            <Link href="/organizations">
+              <div className={c('row')}>
+                <MdBusiness />
+                <span className={c('row-text')}>Organizations</span>
+              </div>
+            </Link>
+
+            <Link href="/employees">
+              <div className={c('row')}>
+                <FaUsers />
+                <span className={c('row-text')}>Employees</span>
+              </div>
+            </Link>
           </div>
+
           <div className={c('footer')}>
             <span className={c('feedback-icon')} />
             <a className={c('feedback-text')}>Send Feedback</a>
