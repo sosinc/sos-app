de8c8cb0f7c8b8ca3f70248ed610c2853db44688
Jatinder <jkhabra09@gmail.com>
2020-06-18 14:30:51 +0530

    feat: fetch user's organization, projects and employees in me API

--- ui/src/entities/User.ts
+++ ui/src/entities/User.ts
@@ -2,6 +2,8 @@ import 'cross-fetch/polyfill';
 
 import client from 'src/lib/client';
 import resolveStorageFile from 'src/utils/resolveStorageFile';
+import { Employee } from './Employee';
+import { Organization } from './Organizations';
 
 export interface Role {
   id: string;
@@ -9,6 +11,7 @@ export interface Role {
 }
 
 export interface User {
+  name: string;
   email: string;
   role: Role;
   avatar?: string;
@@ -31,20 +34,75 @@ export const login = async (payload: LoginPayload): Promise<User> => {
   return client.request(query, payload).then((data) => data.login);
 };
 
+export interface CurrentUserResponse {
+  user: User;
+  employees: Employee[];
+  organizations: Organization[];
+  projects: any[];
+}
+
 /**
  * "me" request for getting loggedIn user on initial page load. `headers` are
  * required when making this request from server-side because server don't send
  * the cookie header automatically.
  */
-export const fetchCurrentUser = async (): Promise<User> => {
-  const query = `query { me { email, role { id, name }} }`;
+export const fetchCurrentUser = async (): Promise<CurrentUserResponse> => {
+  const query = `query {
+    me {
+      id
+      name
+      avatar
+      email
+      as_employee {
+        ecode
+        name
+        organization_id
+        organization {
+          id
+          name
+          projects {
+            id
+            name
+            logo
+          }
+        }
+      }
+      role {
+        id
+        name
+      }
+    }
+   }`;
 
   const data = await client.request(query);
+  const me = data.me.length ? data.me[0] : null;
 
-  return data.me?.length
-    ? {
-        ...data.me[0],
-        avatar: resolveStorageFile(data.me[0].avatar),
-      }
-    : undefined;
+  if (!me) {
+    throw new Error('Could not get current user');
+  }
+
+  const employees = me.as_employee.map((e: any) => ({
+    ecode: e.ecode,
+    name: e.name,
+    organization_id: e.organization_id,
+  }));
+  const organizations = me.as_employee.map((e: any) => ({
+    id: e.organization.id,
+    name: e.organization.name,
+  }));
+  const projects = me.as_employee.flatMap((e: any) =>
+    e.organization.projects.map((p: any) => ({ ...p, employeeId: e.id })),
+  );
+
+  return {
+    employees,
+    organizations,
+    projects,
+    user: {
+      avatar: resolveStorageFile(me.avatar),
+      email: me.email,
+      name: me.name,
+      role: me.role,
+    },
+  };
 };
