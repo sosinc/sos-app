0d12a3af3e0862950ebc90700446ab377d4dcf9f
Jatinder <jkhabra09@gmail.com>
2020-06-21 18:21:49 +0530

    feat: show no-item found in dashboard

--- ui/src/pages/dashboard/index.tsx
+++ ui/src/pages/dashboard/index.tsx
@@ -1,12 +1,15 @@
 import classNames from 'classnames/bind';
 import Link from 'next/link';
 import { MdAdd, MdMoreHoriz, MdRadioButtonUnchecked } from 'react-icons/md';
+import { RiNewspaperLine, RiPlayListAddLine } from 'react-icons/ri';
 
+import NoItemsFound from 'src/components/NoItemsFound';
 import DashboardLayout from 'src/containers/DashboardLayout';
 import style from './style.module.scss';
 
 const c = classNames.bind(style);
 
+const commitments = [];
 const Header: React.FC = () => (
   <div className={c('header')}>
     Today's Commitments
@@ -18,9 +21,30 @@ const Header: React.FC = () => (
   </div>
 );
 
-const CommitmentRow: React.FC = (p) => (
+const CommitmentRow: React.FC = () => (
   <div className={c('commitments-body')}>
-    <div className={c('status-row')}>{p.children}</div>
+    <div className={c('status-row')}>
+      <div className={c('status-row-left')}>
+        <MdMoreHoriz className={c('row-item')} />
+        <span className={c('row-item')}>CHSL-34</span>
+        <MdRadioButtonUnchecked />
+        <span>Fix bug</span>
+      </div>
+      <div className={c('status-row-right')}>
+        <MdRadioButtonUnchecked />
+      </div>
+    </div>
+  </div>
+);
+
+const noTodaysCommitment = (
+  <div className={c('not-found-container')}>
+    <NoItemsFound
+      Icon={RiPlayListAddLine}
+      message=" You haven't added a status update for today."
+      addItemText="Add one"
+      addItemUrl=""
+    />
   </div>
 );
 
@@ -29,17 +53,7 @@ const Dashboard = () => {
     <DashboardLayout title={'Dashboard - Snake Oil Software'} Header={Header}>
       <div className={c('container')}>
         <div className={c('todays-commitments')}>
-          <CommitmentRow>
-            <div className={c('status-row-left')}>
-              <MdMoreHoriz className={c('row-item')} />
-              <span className={c('row-item')}>CHSL-34</span>
-              <MdRadioButtonUnchecked />
-              <span>Fix bug</span>
-            </div>
-            <div className={c('status-row-right')}>
-              <MdRadioButtonUnchecked />
-            </div>
-          </CommitmentRow>
+          {commitments.length ? <CommitmentRow /> : noTodaysCommitment}
         </div>
 
         <div className={c('team-activity')}>
@@ -48,7 +62,16 @@ const Dashboard = () => {
             <span className={c('activity-filter')}>+Filter</span>
           </div>
 
-          <div className={c('activity-feed')}>null</div>
+          <div className={c('activity-feed')}>
+            <div className={c('not-found-container')}>
+              <NoItemsFound
+                Icon={RiNewspaperLine}
+                message=" There is no activity from your team yet"
+                addItemText=""
+                addItemUrl=""
+              />
+            </div>
+          </div>
         </div>
       </div>
     </DashboardLayout>
