3283a811c6ed00bd5d2ea201549c2b538457a3ca
Jatinder <jkhabra09@gmail.com>
2020-06-30 15:49:47 +0530

    feat: create edit team from component

---
+++ ui/src/components/Teams/Create/index.tsx
@@ -0,0 +1,189 @@
+import classNames from 'classnames/bind';
+import { Formik, FormikHelpers, FormikProps } from 'formik';
+/* import { useState } from 'react'; */
+import { AiOutlineTeam } from 'react-icons/ai';
+import * as Yup from 'yup';
+
+import ImageUploadField from 'src/components/Form/ImageUploadField';
+import SelectBox from 'src/components/Form/SelectBox';
+import TextField from 'src/components/Form/TextField';
+import Listing from 'src/components/Listing';
+import NoItemsFound from 'src/components/NoItemsFound';
+import { Employee } from 'src/entities/Employee';
+
+import style from './style.module.scss';
+
+const c = classNames.bind(style);
+
+interface Props {
+  employees: Employee[];
+  isFetchingEmployees?: boolean;
+  values?: CreateTeamFormValues;
+  isFetchingTeam?: boolean;
+  teamId?: string;
+}
+
+const CreateTeamForm: React.FC<FormikProps<CreateTeamFormValues> & Props> = (p) => {
+  return (
+    <div className={c('container')}>
+      <div className={c({ skeleton: p.isFetchingTeam })}>
+        <form className={c('form')} onSubmit={p.handleSubmit}>
+          <div className={c('title-container')}>
+            <h2>{p.teamId ? p.values.name : 'Create Team'}</h2>
+          </div>
+          <div className={c('name-container', 'field-container')}>
+            <span className={c('field-title')}>Name</span>
+            <TextField placeholder="Enter Name" type="text" name="name" />
+          </div>
+          <div className={c('square-logo', 'field-container')}>
+            <span className={c('field-title')}>Square Logo</span>
+            <ImageUploadField className={c('image-container')} type={'file'} name="logo_square" />
+          </div>
+          <div className={c('right-container')}>
+            <div className={c('issue-link-container', 'field-container')}>
+              <span className={c('field-title')}>Issue Link Template</span>
+              <TextField
+                placeholder="Enter issue Link Template"
+                type="text"
+                name="issue_link_template"
+              />
+            </div>
+            <div className={c('pr-link-container', 'field-container')}>
+              <span className={c('field-title')}>Pr Link Template</span>
+              <TextField placeholder="Enter pr link template" type="text" name="pr_link_template" />
+            </div>
+          </div>
+          <button className={c('save-button')} type="submit" disabled={p.isSubmitting}>
+            <div className={c({ 'saving-in': p.isSubmitting })}>
+              {p.isSubmitting ? 'Saving...' : 'Save'}
+              <span />
+            </div>
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+const NoTeamMembers = () => {
+  return (
+    <NoItemsFound
+      Icon={AiOutlineTeam}
+      message="No Team members found"
+      addItemText=""
+      addItemUrl=""
+    />
+  );
+};
+
+const TeamMembers = (p: any) => {
+  const members = [];
+  /* const teamListItems: ListItemProps[] = members.map((t) => ({
+   *   id: t.id,
+   *   logo: t.logo_square,
+   *   subtitle: `0 members`,
+   *   title: t.name,
+   * })); */
+
+  const isProjectTeams = members.length ? <Listing items={[]} /> : NoTeamMembers();
+
+  return (
+    <div className={c('team-member-container', 'form')}>
+      <div className={c('title-container', 'add-team')}>
+        <div className={c('title')}>
+          <h3>Team Members </h3>
+          <span className={c('sub-title')}>Manage this project' team members</span>
+        </div>
+      </div>
+      <div className={c('team-container')}>
+        <div className={c('emp-container', 'field-container')}>
+          <span className={c('field-title')}>Add Team Member</span>
+          <SelectBox
+            className={c('org-add-form')}
+            name="employee_id"
+            options={p.employees}
+            isLoading={p.isFetchingEmployees}
+          />
+        </div>
+
+        {isProjectTeams}
+      </div>
+    </div>
+  );
+};
+
+// ---
+
+export interface CreateTeamFormValues {
+  issue_link_template: string;
+  name: string;
+  pr_link_template: string;
+  logo_square: string;
+  employee_id: string;
+}
+
+const validationSchema = Yup.object().shape({
+  employee_id: Yup.string(),
+  issue_link_template: Yup.string(),
+  logo_square: Yup.string(),
+  name: Yup.string()
+    .min(2, 'Must be 2 characters or more')
+    .max(16, 'Must be 16 characters or less')
+    .required('Required'),
+  pr_link_template: Yup.string(),
+});
+
+interface OuterFormProps extends Props {
+  onSubmit: (
+    values: CreateTeamFormValues,
+    helpers: FormikHelpers<CreateTeamFormValues>,
+  ) => Promise<void>;
+}
+
+const initialValues = {
+  employee_id: '',
+  issue_link_template: '',
+  logo_square: '',
+  name: '',
+  pr_link_template: '',
+};
+
+const OuterForm: React.FC<OuterFormProps> = (p) => {
+  /* const [selectedEmployeeIds, setSelectedEmployeeIds] = useState<string>('lol'); */
+
+  const employees = p.employees.filter((e: any) => {
+    // don't show if this employee is also in selectedEmployees
+    /* return selectedEmployeeIds.find((sid) => sid === e.id); */
+    return e;
+  });
+
+  /* setSelectedEmployeeIds('lol');
+   * console.warn('------selet', selectedEmployeeIds); */
+
+  const InnerForm = (formikProps: FormikProps<CreateTeamFormValues>) => (
+    <CreateTeamForm
+      isFetchingTeam={p.isFetchingTeam}
+      employees={employees}
+      isFetchingEmployees={p.isFetchingEmployees}
+      teamId={p.teamId}
+      {...formikProps}
+    />
+  );
+
+  return (
+    <>
+      <Formik
+        enableReinitialize={true}
+        initialValues={p.values ? p.values : initialValues}
+        validationSchema={validationSchema}
+        onSubmit={p.onSubmit}
+      >
+        {InnerForm}
+      </Formik>
+
+      <TeamMembers employees={employees} />
+    </>
+  );
+};
+
+export default OuterForm;
---
+++ ui/src/pages/projects/[id]/teams/[teamId].tsx
@@ -0,0 +1,104 @@
+import classNames from 'classnames/bind';
+import { FormikHelpers, FormikValues } from 'formik';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useSelector } from 'react-redux';
+
+import CreateTeam, { CreateTeamFormValues } from 'src/components/Teams/Create';
+import DashboardLayout from 'src/containers/DashboardLayout';
+import { RootState } from 'src/duck';
+import { fetchTeam, teamSelector } from 'src/duck/teams';
+import { projectSelector } from 'src/duck/projects';
+import { createTeamAction } from 'src/duck/teams';
+import { useAsyncThunk, useQuery } from 'src/lib/asyncHooks';
+
+import style from './style.module.scss';
+import { fetchEmployees, employeeSelector } from 'src/duck/employees';
+
+const c = classNames.bind(style);
+
+const Header: React.FC = () => {
+  const router = useRouter();
+  const projectId = String(router.query.id);
+  const teamId = String(router.query.teamId);
+  const project = useSelector((state: RootState) => projectSelector.selectById(state, projectId));
+  const team = useSelector((state: RootState) => teamSelector.selectById(state, teamId));
+
+  return (
+    <div className={c('header')}>
+      <span>
+        <Link href="/projects">
+          <a>Projects > </a>
+        </Link>
+        <Link href={`/projects/${projectId}`}>
+          <a> {project?.name || 'Project'} > </a>
+        </Link>
+        {team?.name || 'Team'}
+      </span>
+    </div>
+  );
+};
+
+const TeamDetails: React.FC<FormikValues> = () => {
+  const router = useRouter();
+  const teamId = String(router.query.teamId);
+
+  const [isFetchingTeam] = useQuery(() => fetchTeam({ id: teamId }), {
+    errorTitle: 'Failed to fetch some team details',
+  });
+  const team = useSelector((state: RootState) => teamSelector.selectById(state, teamId));
+
+  const employees = useSelector(employeeSelector.selectAll);
+  const [isFetchingEmployees] = useQuery(fetchEmployees, {
+    errorTitle: 'Failed to fetch some Employees :-(',
+  });
+
+  const [createTeam] = useAsyncThunk(createTeamAction, {
+    errorTitle: 'Failed to create team member',
+    rethrowError: true,
+    successTitle: 'Created successfully',
+  });
+
+  const handleSubmit = async (
+    values: CreateTeamFormValues,
+    helpers: FormikHelpers<CreateTeamFormValues>,
+  ) => {
+    try {
+      helpers.setSubmitting(true);
+      await createTeam(values);
+      helpers.resetForm();
+    } catch (err) {
+      if (/Duplicate project name/i.test(err.message)) {
+        helpers.setFieldError('name', 'An member with same name already exists');
+      }
+      helpers.setSubmitting(false);
+    }
+  };
+
+  const formValues: CreateTeamFormValues = {
+    issue_link_template: team?.issue_link_template || '',
+    logo_square: team?.logo_square || '',
+    name: team?.name || '',
+    pr_link_template: team?.pr_link_template || '',
+    employee_id: '',
+  };
+
+  return (
+    <CreateTeam
+      onSubmit={handleSubmit}
+      isFetchingTeam={isFetchingTeam}
+      values={formValues}
+      isFetchingEmployees={isFetchingEmployees}
+      employees={employees}
+      teamId={teamId}
+    />
+  );
+};
+
+export default () => {
+  return (
+    <DashboardLayout title={'Teams - Snake Oil Software'} Header={Header}>
+      <TeamDetails />
+    </DashboardLayout>
+  );
+};
