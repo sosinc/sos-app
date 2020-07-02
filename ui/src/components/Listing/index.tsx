774cb205d24dae378b43ba6d156afba8b78aac76
Jatinder <jkhabra09@gmail.com>
2020-07-02 13:31:33 +0530

    feat: show delete icon in member listing
    - listing component get a action component, which we can defile other action as well

--- ui/src/components/Listing/index.tsx
+++ ui/src/components/Listing/index.tsx
@@ -1,19 +1,21 @@
 import classNames from 'classnames/bind';
 import Link from 'next/link';
+
 import style from 'src/components/Listing/style.module.scss';
 import FallbackIcon from 'src/containers/FallbackIcon';
 
 const c = classNames.bind(style);
 
-export interface ListItemProps {
+export interface ListingItemProps {
   id: string;
   title: string;
   subtitle: string;
   logo: string;
   href?: string;
+  Actions?: React.FC<{ id: string }>;
 }
 
-const LinkItem: React.FC<ListItemProps> = (i) => {
+const LinkItem: React.FC<ListingItemProps> = (i) => {
   if (i.href) {
     return (
       <Link href={i.href}>
@@ -24,34 +26,45 @@ const LinkItem: React.FC<ListItemProps> = (i) => {
   return <span className={c('title')}>{i.title}</span>;
 };
 
-const ListItem: React.FC<ListItemProps> = (i) => (
-  <div className={c('content')} key={i.id}>
-    <div className={c('fallback-icon')}>
-      <FallbackIcon logo={i.logo} name={i.title} />
+const ListingItem: React.FC<ListingItemProps> = (p) => {
+  const Actions = p.Actions && (
+    <div className={c('action')}>
+      <p.Actions id={p.id} />
     </div>
-    <div className={c('details')}>
-      {LinkItem(i)}
-      <span className={c('subtitle')}>{i.subtitle}</span>
+  );
+
+  return (
+    <div className={c('content')} key={p.id}>
+      <div className={c('fallback-icon')}>
+        <FallbackIcon logo={p.logo} name={p.title} />
+      </div>
+      <div className={c('details')}>
+        {LinkItem(p)}
+        <span className={c('subtitle')}>{p.subtitle}</span>
+      </div>
+
+      {Actions}
     </div>
-  </div>
-);
+  );
+};
 
 const Skeleton = () => (
   <div className={c('skeleton')}>
-    {ListItem({ id: '', logo: '', title: 'ABC', subtitle: '0', href: '' })}
+    {ListingItem({ id: '', logo: '', title: 'ABC', subtitle: '0', href: '' })}
   </div>
 );
 
-interface ListProps {
+interface ListingProps {
   isFetching?: boolean;
-  items: ListItemProps[];
+  items: ListingItemProps[];
+  Actions?: React.FC<{ id: string }>;
 }
 
-const Listing: React.FC<ListProps> = (p) => {
+const Listing: React.FC<ListingProps> = (p) => {
   return (
     <div className={c('container')}>
       {p.isFetching && <Skeleton />}
-      {p.items.map(ListItem)}
+      {p.items.map((i) => ListingItem({ ...i, Actions: p.Actions }))}
     </div>
   );
 };
